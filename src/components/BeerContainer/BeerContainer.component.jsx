f965551c11c27dbbaad4a9e0940a0b8e583e0ae3
shubhankarb180 <shubhankarb180@gmail.com>
2020-10-05 17:24:40 +0530

    style: Responsive Styling added using Antd

--- src/components/BeerContainer/BeerContainer.component.jsx
+++ src/components/BeerContainer/BeerContainer.component.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-
+import { Row, Col, Divider } from 'antd';
 import './BeerContainer.styles.scss';
 import starUnfilled from './../../assets/starUnfilled.png';
 import starFilled from './../../assets/starFilled.png';
 
 export default function BeerContainer({ beerData, addBeer, favBeerList }) {
-
     const checkFavIcon = (id) => {
         if (favBeerList.length) {
             return favBeerList.reduce(
@@ -17,27 +16,34 @@ export default function BeerContainer({ beerData, addBeer, favBeerList }) {
 
 
     return (
-        <div className='beer-container'>
+        <div>
             {
-                beerData.map((beer) =>
-                    <div className='beer-card' key={beer.id}>
-                        <div className="beer-avatar">
-                            <img className='beer-avatar--image' src={beer.image_url} alt="avatar" />
-                        </div>
-                        <div className="beer-info">
-                            {
-                                checkFavIcon(beer.id) ?
-                                    <button className='favIconButton'>
-                                        <img className="favIcon" src={starFilled} alt='favicon' id={beer.id} />
-                                    </button> :
-                                    <button className='favIconButton' onClick={() => addBeer(beer.id)}>
-                                        <img className="favIcon" src={starUnfilled} alt='favicon' id={beer.id} />
-                                    </button>
-                            }
-                            <h4 className='beer-info--name'>{beer.name}</h4>
-                            <p className='beer-info--description'>{beer.description.slice(0, 100)}</p>
-                        </div>
-                    </div>)
+                <Row gutter={{ xs: 16, sm: 16, md: 16, lg: 16 }}>
+                    {
+                        beerData.map((beer) =>
+                            <Col className="gutter-row" xs={24} sm={24} md={24} lg={24} xl={8}>
+                                <div className='beer-card' key={beer.id}>
+                                    <div className="beer-avatar">
+                                        <img className='beer-avatar--image' src={beer.image_url} alt="avatar" />
+                                    </div>
+                                    <div className="beer-info">
+                                        {
+                                            checkFavIcon(beer.id) ?
+                                                <button className='favIconButton'>
+                                                    <img className="favIcon" src={starFilled} alt='favicon' id={beer.id} />
+                                                </button> :
+                                                <button className='favIconButton' onClick={() => addBeer(beer.id)}>
+                                                    <img className="favIcon" src={starUnfilled} alt='favicon' id={beer.id} />
+                                                </button>
+                                        }
+                                        <h4 className='beer-info--name'>{beer.name}</h4>
+                                        <p className='beer-info--description'>{beer.description.slice(0, 100)}</p>
+                                    </div>
+                                </div>
+                            </Col>
+                        )
+                    }
+                </Row>
             }
         </div>
     );
--- src/pages/FavouritesPage/FavouritesPage.jsx
+++ src/pages/FavouritesPage/FavouritesPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState} from 'react';
 
-import {Button} from 'antd';
+import {Row, Col, Button} from 'antd';
 
 export default function FavouritesPage(){
 
@@ -18,23 +18,46 @@ export default function FavouritesPage(){
         console.log(favouriteBeers);
     }
     return(
-        <div className='beer-container'>
+        // <div className='beer-container'>
+        //     {
+        //         favouriteBeers.map((beer) =>
+        //             <div className='beer-card' key={beer.id}>
+        //                 <div className="beer-avatar">
+        //                     <img className='beer-avatar--image' src={beer.image_url} alt="avatar" />
+        //                 </div>
+        //                 <div className="beer-info">
+        //                     <h4 className='beer-info--name'>{beer.name}</h4>
+        //                     <p className='beer-info--description'>{beer.description.slice(0, 100)}</p>
+        //                     <Button type="primary" danger onClick={()=> deleteBeer(beer.id)}>
+        //                         Delete
+        //                     </Button>
+        //                 </div>
+        //             </div>)
+        //     }
+        // </div>
+
+        <div>
+            <Row gutter={{ xs: 16, sm: 16, md: 16, lg: 16 }}>
             {
                 favouriteBeers.map((beer) =>
-                    <div className='beer-card' key={beer.id}>
-                        <div className="beer-avatar">
-                            <img className='beer-avatar--image' src={beer.image_url} alt="avatar" />
+                    <Col className="gutter-row" xs={24} sm={24} md={24} lg={24} xl={8}>
+                        <div className='beer-card' key={beer.id}>
+                            <div className="beer-avatar">
+                                <img className='beer-avatar--image' src={beer.image_url} alt="avatar" />
+                            </div>
+                            <div className="beer-info">
+                                <h4 className='beer-info--name'>{beer.name}</h4>
+                                <p className='beer-info--description'>{beer.description.slice(0, 100)}</p>
+                                <Button type='primary' danger onClick={() => deleteBeer(beer.id)}>Delete Item</Button>
+                            </div>
                         </div>
-                        <div className="beer-info">
-                            <h4 className='beer-info--name'>{beer.name}</h4>
-                            <p className='beer-info--description'>{beer.description.slice(0, 100)}</p>
-                            <Button type="primary" danger onClick={()=> deleteBeer(beer.id)}>
-                                Delete
-                            </Button>
-                        </div>
-                    </div>)
+                    </Col>
+                )
             }
+        </Row>
         </div>
+
+        
     );
     
     
